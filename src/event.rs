f8d975178494f0511715fcba1b8b476e489c1d29
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-06-21 15:38:15 -0400

    adding map pools and store pools for uniswap v3 substreams

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,8 @@
-pub fn is_pair_created_event(sig: &str) -> bool {
+pub fn is_pool_created_event(signature: &str) -> bool {
     /* keccak value for PoolCreated(address,address,uint24,int24,address) */
-    return sig == "783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118";
+    return signature == "783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118";
+}
+
+pub fn is_pool_sync_event(signature: &str) -> bool {
+    return false
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,47 +5,102 @@ use substreams::errors::Error;
 use substreams::{Hex, log, proto, store};
 use substreams_ethereum::pb::eth as ethpb;
 
-#[substreams::handlers::map]
-pub fn map_pairs(blk: ethpb::v1::Block) -> Result<pb::uniswap::Pairs, Error> {
-    let mut pairs = pb::uniswap::Pairs { pairs: vec![] };
+const UNISWAP_V3_FACTORY: &str = "1f98431c8ad98523631ae4a59f267346ea31f984";
 
-    for trx in blk.transaction_traces {
-        /* PCS Factory address */
-        //0x1f98431c8ad98523631ae4a59f267346ea31f984
-        if hex::encode(&trx.to) != "1f98431c8ad98523631ae4a59f267346ea31f984" {
+#[substreams::handlers::map]
+pub fn map_pools(block: ethpb::v1::Block) -> Result<pb::uniswap::Pools, Error> {
+    let mut pools = pb::uniswap::Pools { pools: vec![] };
+
+    // todo: use abigen instead of manually checking the events
+    for trx in block.transaction_traces {
+        /* Uniswap v3 Factory address 0x1f98431c8ad98523631ae4a59f267346ea31f984 */
+        if hex::encode(&trx.to) != UNISWAP_V3_FACTORY {
             continue;
         }
 
         for log in trx.receipt.unwrap().logs {
             let sig = hex::encode(&log.topics[0]);
 
-            if !event::is_pair_created_event(sig.as_str()) {
+            if !event::is_pool_created_event(sig.as_str()) {
                 continue;
             }
 
-            pairs.pairs.push(pb::uniswap::Pair {
+            pools.pools.push(pb::uniswap::Pool {
                 address: Hex(&log.data[12..32]).to_string(),
                 token0_address: Hex(&log.topics[1][12..]).to_string(),
                 token1_address: Hex(&log.topics[2][12..]).to_string(),
                 creation_transaction_id: Hex(&trx.hash).to_string(),
-                block_num: blk.number,
+                fee: Hex(&log.topics[3][12..]).to_string().parse().unwrap(),
+                block_num: block.number,
                 log_ordinal: log.block_index as u64,
             })
         }
-
     }
 
-    Ok(pairs)
+    Ok(pools)
 }
 
 #[substreams::handlers::store]
-pub fn store_pairs(pairs: pb::uniswap::Pairs, output: store::StoreSet) {
-    log::info!("Building pair state");
-    for pair in pairs.pairs {
+pub fn store_pools(pools: pb::uniswap::Pools, output: store::StoreSet) {
+    log::info!("Building pool state");
+    for pool in pools.pools {
         output.set(
-            pair.log_ordinal,
-            format!("pair:{}", pair.address),
-            &proto::encode(&pair).unwrap(),
+            pool.log_ordinal,
+            format!("pool:{}", pool.address),
+            &proto::encode(&pool).unwrap(),
         );
     }
 }
+
+// #[substreams::handlers::map]
+// pub fn map_reserves(
+//     block: ethpb::v1::Block,
+//     pairs: store::StoreGet,
+//     tokens: store::StoreGet
+// ) -> Result<pb::uniswap::Reserves, Error> {
+//     let mut reserves = pb::uniswap::Reserves { reserves: vec![] };
+//
+//     for trx in block.transaction_traces {
+//         for log in trx.receipt.unwrap().logs {
+//             let addr: String = Hex(&log.address).to_string();
+//             match pairs.get_last(&format!("pair:{}", addr)) {
+//                 None => continue,
+//                 Some(pair_bytes) => {
+//                     let sig = hex::encode(&log.topics[0]);
+//
+//                     if !event::is_pair_sync_event(sig.as_str()) {
+//                         continue
+//                     }
+//
+//                 }
+//             }
+//         }
+//     }
+//
+//     Ok(reserves)
+// }
+//
+// #[substreams::handlers::store]
+// pub fn store_reserves(
+//     clock: substreams::pb::substreams::Clock,
+//     reserves: pb::uniswap::Reserves,
+//     pairs_store: store::StoreGet,
+//     output: store::StoreSet
+// ) {
+//
+//     // todo
+//
+// }
+//
+// #[substreams::handlers::store]
+// pub fn store_prices(
+//     clock: substreams::pb::substreams::Clock,
+//     reserves: pb::uniswap::Reserves,
+//     pairs_store: store::StoreGet,
+//     reserves_store: store::StoreGet,
+//     output: store::StoreSet
+// ) {
+//
+//     // todo -> price stream for usd
+//
+// }
