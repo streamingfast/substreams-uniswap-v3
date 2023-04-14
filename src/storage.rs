0aca7cebeebd8e62975aa16d44ed7f8f8b5f9f57
Julien Cassis <juliencassis@gmail.com>
2023-04-14 09:04:06 -0400

    fix broken test

--- src/storage.rs
+++ src/storage.rs
@@ -37,15 +37,6 @@ impl<'a> UniswapPoolStorage<'a> {
             contract_addr: contract_pad(contract_addr),
         };
     }
-    // pub fn new(
-    //     storage_changes: &'a Vec<StorageChange>,
-    //     contract_addr: [u8; 20],
-    // ) -> UniswapPoolStorage<'a> {
-    //     return Self {
-    //         storage_changes,
-    //         contract_addr,
-    //     };
-    // }
 
     pub fn get_fee_growth_global0x128(&self) -> Option<(BigInt, BigInt)> {
         let feeGrowthGlobal0X128_slot = BigInt::from(1);
@@ -282,13 +273,14 @@ pub fn read_bytes(buf: &Vec<u8>, offset: usize, number_of_bytes: usize) -> &[u8]
 #[cfg(test)]
 mod tests {
     use crate::storage::{left_pad, left_pad_from_bigint, read_bytes, UniswapPoolStorage};
-    use std::num::ParseIntError;
+    use std::{fmt::Write, num::ParseIntError};
     use std::ops::Add;
     use std::str::FromStr;
     use substreams::hex;
     use substreams::scalar::BigInt;
     use substreams_ethereum::pb::eth::v2::StorageChange;
     use tiny_keccak::{Hasher, Keccak};
+
     #[test]
     fn left_pad_lt_32_bytes() {
         let input = vec![221u8, 98u8, 237u8, 62u8];
@@ -399,7 +391,7 @@ mod tests {
 
         let storage = UniswapPoolStorage::new(
             &storage_changes,
-            hex!("7858e59e0c01ea06df3af3d20ac7b0003275d4bf"),
+            &hex!("7858e59e0c01ea06df3af3d20ac7b0003275d4bf").to_vec(),
         );
         let v_opt = storage.get_slot0_sqrt_price_x96();
         assert_eq!(
@@ -425,7 +417,7 @@ mod tests {
 
         let storage = UniswapPoolStorage::new(
             &storage_changes,
-            hex!("7858e59e0c01ea06df3af3d20ac7b0003275d4bf"),
+            &hex!("7858e59e0c01ea06df3af3d20ac7b0003275d4bf").to_vec(),
         );
         let tick_idx = BigInt::from(10);
         let v_opt = storage.get_ticks_initialized(&tick_idx);
