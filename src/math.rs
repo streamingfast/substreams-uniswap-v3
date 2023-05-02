41dd7ec2f2304ecf7e24924413b73f6b3fbd811a
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2023-05-02 15:24:12 -0400

    adding test for compute price from tick idx

--- src/math.rs
+++ src/math.rs
@@ -2,6 +2,7 @@ use crate::ticks_idx::ONE_POINT_0001;
 use crate::BigInt;
 use std::borrow::Borrow;
 use std::ops::{Add, Div, Mul};
+use substreams::log;
 use substreams::scalar::BigDecimal;
 
 pub fn big_decimal_exponated(amount: BigDecimal, exponent: i32) -> BigDecimal {
@@ -30,17 +31,25 @@ pub fn big_decimal_exponated(amount: BigDecimal, exponent: i32) -> BigDecimal {
 }
 
 pub fn compute_price_from_tick_idx(desired_tick_idx: i32) -> BigDecimal {
-    let base = desired_tick_idx - (desired_tick_idx % 1000);
-    let ratio = BigDecimal::try_from(1.0001).unwrap().with_prec(34);
-    let mut val = BigDecimal::try_from(*ONE_POINT_0001.get(&base).unwrap()).unwrap();
+    if desired_tick_idx == 0 {
+        return BigDecimal::one();
+    }
 
-    let mut idx = base;
-    while idx <= desired_tick_idx {
+    let base_abs = (desired_tick_idx - (desired_tick_idx % 1000)).abs();
+    let ratio = BigDecimal::try_from(1.0001).unwrap().with_prec(100);
+    let mut val = BigDecimal::try_from(*ONE_POINT_0001.get(&base_abs).unwrap()).unwrap();
+
+    let mut idx = base_abs;
+    while idx < desired_tick_idx {
         val = val.mul(ratio.clone()).with_prec(100);
         idx += 1;
     }
 
-    return val;
+    if desired_tick_idx.lt(&0) {
+        val = safe_div(&BigDecimal::one(), &val).with_prec(100);
+    }
+
+    return val.with_prec(100);
 }
 
 pub fn safe_div(amount0: &BigDecimal, amount1: &BigDecimal) -> BigDecimal {
@@ -63,4 +72,33 @@ pub fn exponent_to_big_decimal(decimals: u64) -> BigDecimal {
     }
 
     return result;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::math::compute_price_from_tick_idx;
+    use std::str::FromStr;
+    use substreams::prelude::BigDecimal;
+
+    #[test]
+    fn test_positive_tick_idx() {
+        let tick_idx = 257820;
+        let actual_value = compute_price_from_tick_idx(tick_idx);
+        let expected_value = BigDecimal::from_str(
+            "157188409912.8279800665572784382799429044388135818770675416117949775512036146406973508030483126972441",
+        )
+        .unwrap();
+        assert_eq!(expected_value, actual_value);
+    }
+
+    #[test]
+    fn test_negative_tick_idx() {
+        let tick_idx = -16200;
+        let actual_value = compute_price_from_tick_idx(tick_idx);
+        let expected_value = BigDecimal::from_str(
+            "0.1979147284588052764428880652056914101428568621377186361720748341060154174725108347202390730358454528",
+        )
+        .unwrap();
+        assert_eq!(expected_value, actual_value);
+    }
 }
